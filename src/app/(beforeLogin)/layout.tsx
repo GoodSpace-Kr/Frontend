1f6eae30a8e462761059998de5e46f4d9e9d94a6
ByeongJu0411 <wjsqudwn981789@gmail.com>
2025-08-11 13:44:21 +0900

    fix: layout.tsx type error

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import styles from "@/app/(beforeLogin)/_component/main.module.css";
 
 type Props = { children: ReactNode; modal: ReactNode };
-export default function Layout({ children }: Props) {
+export default function BeforeLoginLayout({ children }: Props) {
   return <div className={styles.container}>{children}</div>;
 }
 
