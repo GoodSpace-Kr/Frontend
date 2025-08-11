2fc6922e37d0794cedc6c266b7386052f77b5289
ByeongJu0411 <wjsqudwn981789@gmail.com>
2025-08-11 14:21:11 +0900

    fix: beforeLogin layout Props type error

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from "react";
 import styles from "@/app/(beforeLogin)/_component/main.module.css";
 
-type Props = { children: ReactNode; modal: ReactNode };
-
-export default function BeforeLoginLayout({ children, modal }: Props) {
+export default function BeforeLoginLayout({ children, modal }: { children: ReactNode; modal?: ReactNode }) {
   return (
     <div className={styles.container}>
       {children}
